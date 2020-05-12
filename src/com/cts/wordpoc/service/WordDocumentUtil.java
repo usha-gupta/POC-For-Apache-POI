fbf92a8d7bc5353a996f371203785cb0fa0f95e9
Usha Gupta <ushagupta6205@gmail.com>
2020-05-12 15:21:07 +0530

    update latest all issues

--- src/com/cts/wordpoc/service/WordDocumentUtil.java
+++ src/com/cts/wordpoc/service/WordDocumentUtil.java
@@ -20,77 +20,76 @@ public class WordDocumentUtil {
 	
 	private XWPFDocument document; 
 	private XWPFHeaderFooterPolicy policy; 
-	
-	
+		
+		
 	public XWPFDocument create() throws IOException, XmlException { 
 		//Create document
 		this.document = new XWPFDocument(); 		
 		CTSectPr sectPr = this.document.getDocument().getBody().addNewSectPr();
 		this.policy = new XWPFHeaderFooterPolicy(this.document, sectPr);
 		return this.document;
-	
 	}
 	
 	
-		public void save(String fileName) throws IOException { 
-			//save document
-			FileOutputStream out = new FileOutputStream(new File(fileName+".docx"));
-			this.document.write(out);
-		    out.close();
-		}
+	public void save(String fileName) throws IOException { 
+		//save document
+		FileOutputStream out = new FileOutputStream(new File(fileName+".docx"));
+		this.document.write(out);
+		out.close();
+	}
 		
 		
-		public void addTitle(String title, int fontSize, ParagraphAlignment alignment) {
-			//for Title
-			XWPFParagraph paragraphTitle = this.document.createParagraph();
-			XWPFRun addTitle = paragraphTitle.createRun();
-			paragraphTitle.setAlignment(alignment);	  
-			addTitle.setText(title);
-			addTitle.setFontSize(fontSize);
-			addTitle.setBold(true);		
-		}
+	public void addTitle(String title, int fontSize, ParagraphAlignment alignment) {
+		//for Title
+		XWPFParagraph paragraphTitle = this.document.createParagraph();
+		XWPFRun addTitle = paragraphTitle.createRun();
+		paragraphTitle.setAlignment(alignment);	  
+		addTitle.setText(title);
+		addTitle.setFontSize(fontSize);
+		addTitle.setBold(true);		
+	}
 		
 		
-		public void addParagraph(String content, int fontSize) {
+	public void addParagraph(String content, int fontSize) {
 			
-			//write body content
-			//titlePoint.addBreak();  //for line break
-			XWPFParagraph bodyParagraph = this.document.createParagraph();
-			bodyParagraph.setAlignment(ParagraphAlignment.BOTH);
-			XWPFRun addDescription = bodyParagraph.createRun();
-			addDescription.setFontSize(fontSize);
-			addDescription.setText(content);
-		}
+		//write body content
+		//titlePoint.addBreak();  //for line break
+		XWPFParagraph bodyParagraph = this.document.createParagraph();
+		bodyParagraph.setAlignment(ParagraphAlignment.BOTH);
+		XWPFRun addDescription = bodyParagraph.createRun();
+		addDescription.setFontSize(fontSize);
+		addDescription.setText(content);
+	}
 		
-		 public void addParagraph(String content) {
-	            addParagraph(content, 12);
+	public void addParagraph(String content) {
+		  addParagraph(content, 12);
 	      }
 		
 		
-		public void addHeader(String header) throws IOException { 
-			// here you will create header 			
-			CTP ctpHeader = CTP.Factory.newInstance();
-			CTR ctrHeader = ctpHeader.addNewR();
-			CTText ctHeader = ctrHeader.addNewT();
-			ctHeader.setStringValue(header);	
-			XWPFParagraph headerParagraph = new XWPFParagraph(ctpHeader, this.document);
-		    XWPFParagraph[] parsHeader = new XWPFParagraph[1];
-		    parsHeader[0] = headerParagraph;
-		    this.policy.createHeader(XWPFHeaderFooterPolicy.DEFAULT, parsHeader);
-		}
+	public void addHeader(String header) throws IOException { 
+		// here you will create header 			
+		CTP ctpHeader = CTP.Factory.newInstance();
+		CTR ctrHeader = ctpHeader.addNewR();
+		CTText ctHeader = ctrHeader.addNewT();
+		ctHeader.setStringValue(header);	
+		XWPFParagraph headerParagraph = new XWPFParagraph(ctpHeader, this.document);
+	    XWPFParagraph[] parsHeader = new XWPFParagraph[1];
+	    parsHeader[0] = headerParagraph;
+	    this.policy.createHeader(XWPFHeaderFooterPolicy.DEFAULT, parsHeader);
+	}
 		
 
-		public void addFooter(String footer) throws IOException {
-			//write footer content
-			CTP ctpFooter = CTP.Factory.newInstance();
-			CTR ctrFooter = ctpFooter.addNewR();
-			CTText ctFooter = ctrFooter.addNewT();
-			ctFooter.setStringValue(footer);	
-			XWPFParagraph footerParagraph = new XWPFParagraph(ctpFooter, this.document);
-			XWPFParagraph[] parsFooter = new XWPFParagraph[1];
-			parsFooter[0] = footerParagraph;
-			this.policy.createFooter(XWPFHeaderFooterPolicy.DEFAULT, parsFooter);
-			}
+	public void addFooter(String footer) throws IOException {
+		//write footer content
+		CTP ctpFooter = CTP.Factory.newInstance();
+		CTR ctrFooter = ctpFooter.addNewR();
+		CTText ctFooter = ctrFooter.addNewT();
+		ctFooter.setStringValue(footer);	
+		XWPFParagraph footerParagraph = new XWPFParagraph(ctpFooter, this.document);
+		XWPFParagraph[] parsFooter = new XWPFParagraph[1];
+		parsFooter[0] = footerParagraph;
+		this.policy.createFooter(XWPFHeaderFooterPolicy.DEFAULT, parsFooter);
+	}
 
 		
 	
